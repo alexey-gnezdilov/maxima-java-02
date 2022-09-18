8af0916ceecb870352971545e981212f95c7972b
alexey-gnezdilov <gnezdilov_alexey_1993@mail.ru>
2022-09-18 21:03:37 +0800

    Инкапсуляция. Описание класса, свойств, методов.

---
+++ src/main/java/org/example/Transport.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public class Transport {
+    String name;
+    int capacity;
+    int speed;
+    float costOfKm;
+
+    public Transport(String name, int capacity, int speed, float costOfKm) {
+        this.name = name;
+        this.capacity = capacity;
+        this.speed = speed;
+        this.costOfKm = costOfKm;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public float getCostOfKm() {
+        return costOfKm;
+    }
+
+    public void setCostOfKm(float costOfKm) {
+        this.costOfKm = costOfKm;
+    }
+
+    public float getPrice(City city) {
+        float price = city.distanceKm * costOfKm;
+        return price;
+    }
+}
