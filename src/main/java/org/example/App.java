aecadcb4e7bc4af1130f0ec2917600bce758f57d
alexey-gnezdilov <gnezdilov_alexey_1993@mail.ru>
2022-09-26 20:03:28 +0800

    ДЗ по логистике

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,13 +2,8 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
-        City irkutsk = new City("Irkuts", 250, true, true);
-
-        Transport truck = new Truck("truck", 500, 120, 50.0f);
-        Transport plane = new Plane("plane", 1000, 500, 10.0f);
-        Transport ship = new Ship("ship", 2000, 240, 500.0f);
-
-        Logistics log = new Logistics(truck, plane, ship);
-        System.out.println(log.getShipping(irkutsk, 20000, 24));
+        City irkutsk = new City("Irkutsk", 250, true, false);
+        System.out.println(TransportFactory.getTransport(irkutsk, 425, 64));
     }
+
 }
